611951b7227f4c99f59a0782d257e8fdcbf02e7b
Peter Fabricius <info@peter-fabricius.de>
2020-06-15 09:48:41 +0200

    added comments to run.sh and README

--- resources/run.sh
+++ resources/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+#
+# When stopping the running hop-server container with 'docker stop' it ended up with an timeout
+# and an exitcode > 0 because signals are not catched correctly.
+# It was not possible to end the container gracefully with Ctrl-C when it was started without -d 
+# option which might be annoying on a gratefull exit. Therefore this script ( run.sh ) 
+# that catches signals coming from the docker host is introduced.
+#
+
 log() {
     echo `date '+%Y/%m/%d %H:%M:%S'`" - ${1}"
 }
